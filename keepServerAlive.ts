2aed01f2c904f44935d1c73eabbe80f6dc430a06
ocsoares <casoares016@gmail.com>
2023-05-01 15:33:14 -0300

    chore: fix to deploy

--- keepServerAlive.ts
+++ keepServerAlive.ts
@@ -7,12 +7,10 @@ console.log('Cron job running');
 
 // Run every 5 minutes to keep server alive
 new CronJob(
-    // '*/5 * * * *',
-    '*/10 * * * * *',
+    '*/5 * * * *',
     async () => {
         try {
-            // await axios.get(url);
-            console.log('Imprimindo CRON !!!');
+            await axios.get(url);
         } catch (error) {
             console.log(error);
         }
